6592810b7c7e3943c8636a24b63be598057ee6ca
manavdutt <mrdutt1701@gmail.com>
2026-09-06 18:59:35 +0530

    Fix: send login email in background thread

--- smartquiz_project/settings.py
+++ smartquiz_project/settings.py
@@ -270,9 +270,11 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Email Settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_TIMEOUT = 10
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = f'StudySuite AI <{os.getenv("EMAIL_HOST_USER", "")}>'
