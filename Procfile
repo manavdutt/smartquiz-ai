08bdd499020b15b0c7886d073c9317788676cb41
manavdutt <mrdutt1701@gmail.com>
2026-09-06 14:33:14 +0530

    Deploy fixes complete

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn smartquiz_project.wsgi:application
--- smartquiz_project/settings.py
+++ smartquiz_project/settings.py
@@ -167,12 +167,20 @@ WSGI_APPLICATION = 'smartquiz_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import dj_database_url
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
